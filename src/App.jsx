bbdcad6e1702cea1bfdf8eb58fe33994526dfdd0
DavidCirdei <david.cirdei2134@gmail.com>
2025-01-29 23:39:50 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+import { ExploreButton } from '../../components/ExploreButton';
+
+
+
+
+function App() {
+  
+
+  return (
+    <div className="App"> 
+     <div className="SearchBar">
+    <ExploreButton />
+     <div>SearchResults</div>
+     </div>
+
+
+    </div>
+  
+    
+
+);
+  }
+
+export default App
